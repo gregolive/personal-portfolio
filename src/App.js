ea7dc6483f20fd1797dafd66c70f0e3a2b662fc1
Gregory Olive <gregoryolive@outlook.com>
2022-04-25 16:46:53 +0900

    Recreate header in react

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Header from './components/header/Header'
+
+const App = () => {
+  return (
+    <div className='App'>
+      <Header />
+    </div>
+  );
+};
+
+export default App;
---
+++ src/components/header/Header.js
@@ -0,0 +1,26 @@
+import { useState } from 'react';
+import './Header.css';
+
+const Header = () => {
+  const [menu, setMenu] = useState(false);
+
+  const toggleMenu = () => setMenu(!menu);
+
+  return (
+    <nav className='navbar'>
+      <a href='/' className='navbar-title'>go</a>
+
+      <button className='navbar-burger' onClick={toggleMenu}>
+        <svg xmlns='http://www.w3.org/2000/svg' version='1.1' width='24' height='24' viewBox='0 0 24 24'><path d='M3,6H21V8H3V6M3,11H21V13H3V11M3,16H21V18H3V16Z' /></svg>
+      </button>
+
+      <div className={(menu) ? 'navbar-menu open' : 'navbar-menu close'}>
+        <a href='#bio' className='navbar-item navbar-link'>Bio</a>
+        <a href='#projects' className='navbar-item navbar-link'>Projects</a>
+        <a href='#open-source' className='navbar-item navbar-link'>Open source</a>
+      </div>
+    </nav>
+  );
+};
+
+export default Header;
