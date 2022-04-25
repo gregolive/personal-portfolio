ea5092304b143c69798ea21791dd8d688d46cd0d
Gregory Olive <gregoryolive@outlook.com>
2022-04-25 16:56:57 +0900

    Build footer in react

--- old/js/script.js
+++
@@ -1,8 +0,0 @@
-function openMenu() {
-  let links = document.querySelector(".navbar-menu");
-  if (links.style.display === "grid") {
-    links.style.display = "none";
-  } else {
-    links.style.display = "grid";
-  }
-}
--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
-import Header from './components/header/Header'
+import Header from './components/header/Header';
+import Footer from './components/footer/Footer';
 
 const App = () => {
   return (
     <div className='App'>
       <Header />
+      <Footer />
     </div>
   );
 };
